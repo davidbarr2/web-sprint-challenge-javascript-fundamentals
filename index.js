d7c29299d725701b51973073fcd102685370ca8e
David - Lambda School User <david-lambdaschooluser@davids-mbp.lan>
2021-09-17 09:47:41 -0400

    all tests passing. sprint complete.

--- index.js
+++ index.js
@@ -189,16 +189,27 @@ function CuboidMaker(attrs){
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
  
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+  constructor(attrs){
+    this.length = attrs.length
+    this.width = attrs.width
+    this.height = attrs.height
+  }
+  volume(){
+    return this.length*this.width*this.height
+  }
+  surfaceArea(){
+    return 2*(this.length*this.width+this.length*this.height+this.width*this.height)
+  }
 }
 
+const cuboidTwo = new CuboidMakerTwo( {"length": 4,"width": 5 ,"height": 5} )
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
 // console.log(cuboidTwo.volume()); // 100
